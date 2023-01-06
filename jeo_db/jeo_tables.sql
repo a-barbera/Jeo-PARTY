89fa5de23b5df27ac6f76303fd206108d3543818
PeteW76 <peterwarren76@gmail.com>
2023-01-05 20:33:55 -0500

    initial sql and json import.

---
+++ jeo_db/jeo_tables.sql
@@ -0,0 +1,15 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+DROP TABLE IF EXISTS contestants;
+CREATE TABLE "contestants" (
+    "contestant_id" int   NOT NULL,
+    "name" varchar   NOT NULL,
+    "notes" varchar   NOT NULL,
+    "games_played" int   NOT NULL,
+    "total_ winnings" int   NOT NULL,
+    CONSTRAINT "pk_contestants" PRIMARY KEY (
+        "contestant_id"
+     )
+);
+
