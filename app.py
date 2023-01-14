076f5c0281047ad37f055b747a3d6e9a9c6ecf0e
TobiAdel <Tobi.adel@yahoo.com>
2023-01-14 13:14:45 -0500

    Merging Visulization

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, url_for, redirect, request, session, f
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
+import plotly.express as px
+import plotly.graph_objects as go
+from dash import Dash, dcc, html, Input, Output
+import plotly
+import json
+import pandas as pd
+
 
 import numpy as np
 
@@ -20,18 +27,28 @@ app.secret_key = "secret"
 #=======================================================================
 
 # adding connection to database. file name is name of the table
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/jeoparty'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Daddy1995!@localhost/jeoparty'
 # this code removes a warning
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # creating a database
 db = SQLAlchemy(app)
 
+
 class Categories(db.Model):
     __tablename__ = 'categories'
     index = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(1000))
     clue_count = db.Column(db.Integer)
+
+class Contestants(db.Model):
+    __tablename__ = 'contestants'
+    contestant_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    notes = db.Column(db.String(1000))
+    games_played = db.Column(db.Integer)
+    total_winnings = db.Column(db.Integer)
+    
 
 #=======================================================================
 
@@ -44,8 +61,76 @@ def home():
 
 @app.route("/stats", methods=['GET'])
 def stats():
+#Graph One Categories
     categories = Categories.query.all()
-    return render_template("stats.html", categories=categories)
+    result_dict = [u.__dict__ for u in Categories.query.all()]
+    df = pd.DataFrame(result_dict).head(10)
+    #print(df)
+
+    fig1 =px.bar(df, x = "clue_count", y = "category", orientation='h',color= "category", text_auto='.2s')
+    fig1.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False, )
+    fig1.update_layout(  yaxis={'categoryorder':'total ascending'}, title_text='Jeopardy! Top 10 Most Common Categories', title_x=0.5, xaxis_title=" Category Count", yaxis_title="Categories")
+   
+    graph1JSON = json.dumps(fig1, cls =plotly.utils.PlotlyJSONEncoder)
+
+
+
+#Graph Two  Contestant
+    contestants = Contestants.query.all()
+    contestants_dict = [u.__dict__ for u in Contestants.query.all()]
+    contestants_dict_df = pd.DataFrame(contestants_dict).head(10)
+    print(contestants_dict_df)
+
+   
+
+    trace_high = go.Bar( x=contestants_dict_df.name,
+                         y=contestants_dict_df.games_played,
+                         name='Games Played',
+                         
+                         
+                         
+                         )
+          
+
+    trace_low = go.Bar( x=contestants_dict_df.name,
+                        y=contestants_dict_df.total_winnings,
+                        name='Total Winnings',
+
+                       
+                        )
+
+    data = [trace_high,trace_low]
+
+
+    layout = list([
+        dict(active=0,
+            buttons=list([   
+                dict(label = 'Games Played',
+                    method = 'restyle',
+                    args = [{'visible': [True, False]},
+                            {'title': 'Jeopardy! Top 10 Leaderboard of Legends'}]),
+
+                dict(label = 'Total Winnings',
+                    method = 'restyle',
+                    args = [{'visible': [False, True]},
+                            {'title': 'Jeopardy! Top 10 Leaderboard of Legends'}]),
+
+            ]),
+        )
+    ])
+
+    layout = go.Layout( xaxis={'title': 'Contestants' },yaxis={'title': 'Leaderboard Count' },
+                        title_text='Jeopardy! Top 10 Leaderboard of Legends', title_x=0.5, barmode = 'stack', height= 600
+                         )
+    
+   
+    my_fig = dict(data = data , layout=layout)
+    fig = go.Figure(my_fig)
+    fig.update_xaxes(categoryorder="total descending")
+    
+    fig5 = json.dumps(fig, cls =plotly.utils.PlotlyJSONEncoder)
+    return render_template("stats.html", graph1JSON = graph1JSON, fig5=fig5)
+
 
 
 @app.route("/game")
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,21 +1,21 @@
-let name = 'Travis Taylor'
+// let name = 'Travis Taylor'
 
-let title = `${name}'s First Plotly Chart`
+// let title = `${name}'s First Plotly Chart`
 
-let books = ["The Visual Display of Quantitative Information", "Automate the Boring Stuff", "Data Science from Scratch"]
+// let books = ["The Visual Display of Quantitative Information", "Automate the Boring Stuff", "Data Science from Scratch"]
 
-let timesRead = [100, 50, 25]
+// let timesRead = [100, 50, 25]
 
-let trace1 = {
-  x: books,
-  y: timesRead,
-  type: 'bar'
-};
+// let trace1 = {
+//   x: books,
+//   y: timesRead,
+//   type: 'bar'
+// };
 
-let data = [trace1];
+// let data = [trace1];
 
-let layout = {
-  title: title
-};
+// let layout = {
+//   title: title
+// };
 
-Plotly.newPlot("plot", data, layout);
+// Plotly.newPlot("plot", data, layout);
